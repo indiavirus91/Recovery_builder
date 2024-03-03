40c525b43d9dd05ce775a43b18219df11f9694bf
indiavirus91 <160828583+indiavirus91@users.noreply.github.com>
2024-03-03 20:00:00 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,11 +6,11 @@ export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp
 export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
-export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
+export DEVICE="tb8789p1_64" # codename used in device tree
+export DT_LINK="https://github.com/twrpdtgen/android_device_motorola_tb8789p1_64" # device tree link
+export DT_BRANCH="full_tb8789p1_64-user-12-SP1A.210812.016-g70_LTE_S200139_230411_ROW-release-keys" # device tree branch
+export VENDOR="motorola" # device manufacturer or vendor
+export TARGET="bootimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
